9e47b1077d93ff9bbd311896eb7282307c5a044a
Patrick Berndt <122051407+PatrickBerndt@users.noreply.github.com>
2023-02-19 14:29:18 +0100

    reconfigert arrangement

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let users = [];
-let contacts = [];
 let tasks = [];
 
 async function init() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
-    contacts = JSON.parse(backend.getItem('contacts')) || [];
     tasks = JSON.parse(backend.getItem('tasks')) || [];
 }
