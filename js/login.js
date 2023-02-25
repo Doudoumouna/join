25d2052964e353f3a1cce019b8409f3963ba198b
Patrick Berndt <122051407+PatrickBerndt@users.noreply.github.com>
2023-02-25 19:01:48 +0100

    bugs fixed

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  let resetUserMail = params.some_key; // "some_value"
+
 function logIn(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginPassword = document.getElementById('loginPassword').value;
@@ -20,6 +28,11 @@ function logIn(){
         }
     }
 }
+ 
+function unsetCurrentUser(){
+    localStorage.setItem('currentUser','')
+    document.getElementById('initialHeader').classList.add('dNone');
+}
 
 function remember(){
     if(localStorage.getItem('remember')){
@@ -33,8 +46,26 @@ function remember(){
 
 function renderHeadInitials(){
     let currentUser = localStorage.getItem('currentUser')
-    let user = users[currentUser];
-    document.getElementById('initialHeader').classList.remove('dNone');
-    document.getElementById('initialHeader').innerHTML = `${user['initial']}`;
-    document.getElementById('initialHeader').style.backgroundColor = user['color']; 
+    if(currentUser != ''){
+        let user = users[currentUser];
+        document.getElementById('initialHeader').classList.remove('dNone');
+        document.getElementById('initialHeader').innerHTML = `${user['initial']}`;
+        document.getElementById('initialHeader').style.backgroundColor = user['color']; 
+    }
+}
+
+
+async function resetUserPassword(){
+    let pw1 = document.getElementById('reset1Password').value;
+    let pw2 = document.getElementById('reset2Password').value;
+    if(pw1 ==pw2){
+        for (let i = 0; i < users.length; i++) {
+            const user = users[i];
+            if(resetUserMail == user['email']){
+                user = {'firstName':firstName,'lastName':lastName,'email':email,'password':user['password'],'color':user['color'],'phone':tel,'initial':initials};
+                users[i]=user;
+                await backend.setItem('users', JSON.stringify(users));
+            }
+        }
+    }
 }
